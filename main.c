65ab2d6a50151676d25cd6beae07ed5aa5ac3ca0
elliot <elliotzen256@gmail.com>
2023-09-17 20:51:38 +0800

    :bulb: first commit

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <limits.h>
+#include <stdio.h>
+#include "show_bytes.h"
+
+int main() {
+	show_int(0x87654321);
+	return 0;
+}
---
+++ utils/show_bytes.c
@@ -0,0 +1,8 @@
+#ifndef CSAPP_UTILS_H
+#define CSAPP_UTILS_H
+
+void show_int(int);
+void show_float(float);
+void show_pointer(void*);
+
+#endif
